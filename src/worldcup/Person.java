aea81e618bb4c35a82eb4e94c9a271e5e3010145
Joan735 <joanye777@gmail.com>
2025-09-17 17:09:41 +0200

    Create the class Person

---
+++ src/worldcup/Person.java
@@ -0,0 +1,27 @@
+package worldcup;
+
+/**
+ * Class to define the Person
+ */
+public abstract class Person {
+	int age;
+	
+	
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public void run() {
+		System.out.println("running");
+	}
+}
