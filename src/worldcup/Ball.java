6815a4e5f62d94dc8698107246aef71e55fa3149
Junxi-HM <em.chen.junxi@gmail.com>
2025-09-18 15:41:55 +0200

    fix #17 A constructor Ball(), has to be defined.

--- src/worldcup/Ball.java
+++ src/worldcup/Ball.java
@@ -11,10 +11,16 @@ public class Ball {
 		this.material = material;
 	}
 
+	public Ball() {
+		this.material = "Polyvinyl chloride";
+	}
+	
 	public Ball(String material) {
 		super();
 		this.material = material;
 	}
+
+	
 
 	public void move() {
 		System.out.println(" moving the ball");
